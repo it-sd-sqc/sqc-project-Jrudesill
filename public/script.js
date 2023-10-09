e2cd8d0f324ab42c2c2dddc4f1a54d99b9fd2997
jrudesill <jenniferrudesill@gmail.com>
2023-10-09 14:05:04 -0600

    Redeveloped the light and dark themes and completed a nice CSS stylesheet update for these.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,50 @@
-const toggleButton = document.getElementById('toggle-mode');
-const body = document.body;
+//Get the HTML element that will be toggled between light and dark modes
+const body = document.body
 
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark');
-});
+//Check if the user has a preference for light or dark mode
+const prefersDarkMode = window.mathMedia('(prefers-color-scheme: dark)').matches
+
+//Function to toggle between light and dark modes
+function toggleTheme() {
+
+    //Check the current theme and toggle it 
+    if (body.classList.contains('dark-them')) {
+        body.classList.remove('dark-theme')
+        body.classList.add('light-theme')
+        body.classList.setItem('theme', 'light')
+
+    } else {
+        body.classList.remove('light-theme')
+        body.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark')
+    }
+
+}
+
+//Function to set the theme based on user preferences or stored preference
+function setTheme() {
+    if (prefersDarkMode) {
+        body.classList.add('dark-theme')
+    } else {
+        body.classList.add('light-theme')
+    }
+
+    //Check if there's a stored preference and apply it
+    const storedTheme = localStorage.getItem('theme')
+    if (storedTheme === 'dark') {
+        body.classList.add('dark-theme')
+
+    } else if (storedTheme === 'light') {
+      body.classList.add('light-theme')
+    }
+}
+
+//Add an event listener to a button or element to trigger the theme toggle
+const toggleButton = document.getElementById('toggle-theme-button')
+if (toggleButton) {
+    toggleButton.addEventListener('click' , toggleTheme)
+}
+
+
+//Initialize the theme
+setTheme();
